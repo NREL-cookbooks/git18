60a672efbf3a7ced432a1d8cd50f177ad4aa2b31
Nick Muerdter <nick.muerdter@nrel.gov>
2014-03-03 16:35:33 -0700

    Fix for other cookbooks that might install the "git" package directly.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name             'git18'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Nick Muerdter'
+maintainer_email 'nick.muerdter@nrel.gov'
+license          'MIT'
 description      'Installs/Configures git18'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'git'
+depends 'yum-ius'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,19 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Monkey patch the default git cookbook to uninstall the normal git package. We
-# do it this way, so that other recipes can still depend on the more general
-# default "git" cookbook, but we'll still do the right thing if this cookbook
-# gets included.
+include_recipe "yum-ius"
+
+package "old_git_cleanup" do
+  package_name "git"
+  action :remove
+end
+
+# Monkey patch the default git cookbook, so that other recipes can still depend
+# on the more general, default "git" cookbook. This will also fix other
+# cookbooks that might try to install the "git" package on their own.
 chef_gem "chef-rewind"
 require "chef/rewind"
 
 include_recipe "git"
 
 rewind "package[git]" do
-  action :remove
+  package_name "git18"
 end
-
-# Install git 1.8 out of the ius repo.
-include_recipe "yum-ius"
-package "git18"
